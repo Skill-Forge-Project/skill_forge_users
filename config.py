b06d0cb49cf78b112da9a960bf329f65f4519bb3
karastoyanov on macbookPRO <karastoqnov.alexandar@gmail.com>
2025-07-06 23:26:52 +0300

    <enhancement>(Update Config class)

--- config.py
+++ config.py
@@ -8,3 +8,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    
+    # JWT settings
+    JWT_TOKEN_LOCATION = ["cookies"]
+    JWT_COOKIE_SECURE = False          # True in production (use HTTPS)
+    JWT_COOKIE_HTTPONLY = True         # True in production (to prevent JavaScript access)
+    JWT_COOKIE_SAMESITE = "Strict"
+    JWT_ACCESS_COOKIE_PATH = "/"
+    JWT_REFRESH_COOKIE_PATH = "/refresh_access_token"
